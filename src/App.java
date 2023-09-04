5529cd43d4c38b0567ba7e60b1336c12bd56964f
Hugo Frimodig <Hugo.Frimodig@elev.ga.ntig.se>
2023-09-04 09:40:44 +0200

    lagt till modulo kod

--- src/App.java
+++ src/App.java
@@ -15,5 +15,11 @@ public class App {
         System.out.println("produkten av tal1 * tal2 är "+produkt);
         System.out.println("kvoten av tal1 / tal2 är "+kvot);
 
+        int rest1 = 10%3;
+        int rest2 = 9%3;
+
+        System.out.println("Resten av 10/3 är"+rest1);
+        System.out.println("Resten av 9/3 är"+ rest2);
+
     }
 }
